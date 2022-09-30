83f448e58b5b7174cb8dae7bac62463fa47f5067
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-09-30 23:24:10 +0900

    Update fibonacci memo

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-  if (n === 0){
-    return 0;
-  }else if(n === 1){
-    return 1;
+  //既に計算済みの場合はマップに値があるので、その値を取ってくる(メモ化)
+  if (memo.has(n)) {
+    return memo.get(n);
   }
-  return fib(n - 1) + fib(n - 2);
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value); //計算済みの値を保存
+  return value;
 }
-
 const length = 40;
-for (let i = 0; i< length; i++){
+for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
